04680886ba76d3bbe60046580fcdc8ff252cee50
bob <bob@example.com>
2012-11-16 18:04:54 +0000

    Added menu to base.html and fixed player.urls

--- player/urls.py
+++ player/urls.py
@@ -3,9 +3,9 @@ from django.views.generic.simple import direct_to_template
 from player.views import *
 
 urlpatterns = patterns('',
-	url(r'^signup/',SignUp),
-	url(r'^login/', LogInRequest),
-	url(r'^logout/',LogOutRequest),
+	url(r'^signup/$',SignUp),
+	url(r'^login/$', LogInRequest),
+	url(r'^logout/$',LogOutRequest),
 	url(r'^resetpassword/passwordsent/$','django.contrib.auth.views.password_reset_done'),
 	url(r'^resetpassword/$','django.contrib.auth.views.password_reset'),
 	url(r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$','django.contrib.auth.views.password_reset_confirm'),
