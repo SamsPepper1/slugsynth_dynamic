dc57d237505a437c2fc38a958a21739c94edf093
bob <bob@example.com>
2012-11-17 02:14:08 +0000

    changed env values to FloatField. added to_json function

--- slugs/models.py
+++ slugs/models.py
@@ -1,20 +1,34 @@
 from django.db import models
 from player.models import Player
 from slugs import helpers
+import json
 
 class Shape(models.Model):
 	name = models.CharField(max_length=30)
 	last_used = models.DateTimeField()
 	shape = models.CharField(max_length=100)
-	attack = models.DecimalField(max_digits=4, decimal_places = 3)
-	decay = models.DecimalField(max_digits=4, decimal_places = 3)
-	sustainLevel = models.DecimalField(max_digits=4, decimal_places = 3)
-	release = models.DecimalField(max_digits=4, decimal_places = 3)
-	sustainLengthDefault = models.DecimalField(max_digits=4, decimal_places = 3)
+	attack = models.FloatField()
+	decay = models.FloatField()
+	sustainLevel = models.FloatField()
+	release = models.FloatField()
+	sustainLengthDefault = models.FloatField()
 
 	def __unicode__(self):
 		return self.name
 
+	def as_data(self):
+		data = {
+			'name': self.name,
+			'A': self.attack,
+			'D': self.decay,
+			'S': self.sustainLevel,
+			'R': self.release,
+			'shape': self.shape}
+		return data
+
+
+	def as_json(self):
+		return json.dumps(self.as_data())
 
 
 class Slug(models.Model):
@@ -27,6 +41,20 @@ class Slug(models.Model):
 	def __unicode__(self):
 		return self.name
 
+	def as_data(self):
+		data = {
+			'owner': self.owner.__unicode__(),
+			'name': self.name,
+			'birthday': self.birthday.ctime(),
+			'color': self.color,
+			'shapes': [s.as_data() for s in self.shapes.all()],
+			'sound': self.sound.as_data(),
+			}
+		return data
+
+	def as_json(self):
+		return json.dumps(self.as_data())
+	
 class Sound(models.Model):
 	WAVEFORM_CHOICES = (
 		('si', 'sine'),
@@ -40,3 +68,15 @@ class Sound(models.Model):
 
 	def __unicode__(self):
 		return self.slug.name +"'s Sound"
+
+	def as_data(self):
+		data = {
+			'waveForm': self.waveForm,
+			'amp': self.amp,
+			'overTones': self.overTones,
+			'noise': self.noise,
+			}
+		return data
+
+	def as_json(self):
+		return json.dumps(self.as_data())
